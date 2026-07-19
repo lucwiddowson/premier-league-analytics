e88a22e7398e7c094d39d7e34fe265c1d25eeeb9
lucwiddowson <luc.widdowson@student.manchester.ac.uk>
2026-07-19 04:34:24 +0100

    Add filtering and sorting queries

--- sql/queries.sql
+++ sql/queries.sql
@@ -12,3 +12,42 @@ ORDER BY year_founded ASC;
 SELECT team_name, city, stadium
 FROM teams
 ORDER BY team_name ASC;
+
+SELECT team_name, year_founded
+FROM teams
+WHERE year_founded < 1900;
+
+SELECT team_name, city
+FROM teams
+WHERE city != 'London';
+
+SELECT team_name, year_founded
+FROM teams
+WHERE year_founded BETWEEN 1880 AND 1900;
+
+SELECT team_name, city, year_founded
+FROM teams
+WHERE city = 'London'
+AND year_founded < 1900;
+
+SELECT team_name, stadium
+FROM teams
+WHERE year_founded >= 1892 
+ORDER BY year_founded DESC;
+
+SELECT team_name, city
+FROM teams
+WHERE city = 'London'
+OR city = 'Manchester';
+
+SELECT team_name, city
+FROM teams
+WHERE city IN ('London', 'Manchester');
+
+SELECT team_name
+FROM teams
+WHERE team_name LIKE '%United%';
+
+SELECT team_name, stadium
+FROM teams
+WHERE stadium LIKE '%Stadium%';
