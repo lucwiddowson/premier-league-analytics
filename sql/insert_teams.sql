7061567203c1398b8620b9428f05df61f28eddcb
lucwiddowson <luc.widdowson@student.manchester.ac.uk>
2026-07-19 04:09:29 +0100

    Add initial Premier League teams

---
+++ sql/insert_teams.sql
@@ -0,0 +1,29 @@
+INSERT INTO teams (
+    team_id,
+    team_name,
+    city,
+    stadium,
+    year_founded
+)
+VALUES (
+    1,
+    'Arsenal',
+    'London',
+    'Emirates Stadium',
+    1886
+);
+
+INSERT INTO teams (
+    team_id,
+    team_name,
+    city,
+    stadium,
+    year_founded
+)
+VALUES (
+    2,
+    'Liverpool',
+    'Liverpool',
+    'Anfield',
+    1892
+);
